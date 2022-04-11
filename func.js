7113a337c00968acb6c9be93f0a5c551aa4977dc
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-10 21:22:53 -0300

    Update func.js

--- func.js
+++ func.js
@@ -1,16 +1,40 @@
 
-let itens = [];
 
-function adicionarNaLista() {
-    let valorDoInput = document.getElementById("adicionar").value
+let itens = [];
+let valorDoStorege =localStorage.getItem("intesDaLista")
+          if(valorDoStorege == null){
+              valorDoStorege = []
+            valorDoStorege.push(itens)             
+              }  
+  
+
+function adicionarNaLista() {         
+let valorDoInput = document.getElementById("adicionar").value
+itens.push(valorDoInput)
+localStorage.setItem("intesDaLista",itens)
+console.log(itens)}
+
+for (const item of valorDoStorege) {
     let itemDaLista = document.createElement("li")
-    itemDaLista.innerText = valorDoInput
-    itens.push(itemDaLista)
+    itemDaLista.innerText = item
     document.getElementById("listagem").appendChild(itemDaLista)
-    console.log(itens)
-    
 }
 
-//const valor = document.getElementById("adicionar").value
-const localStorege =JSON.parse(localStorage.setItem('valor'))
-let valor = localStorage.getItem()
+
+
+
+
+
+
+
+// function adicionarNaLista() {
+    
+//     let valorDoInput = document.getElementById("adicionar").value
+//     let itemDaLista = document.createElement("li")
+//     itemDaLista.innerText = valorDoInput
+//     itens.push(valorDoInput)
+//     document.getElementById("listagem").appendChild(itemDaLista)
+    
+//      
+// }
+
